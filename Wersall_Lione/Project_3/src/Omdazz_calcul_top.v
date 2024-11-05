8f2ca65b8a0c045bdd61168e45baed5419e747d4
WersallLione <shamq623@gmail.com>
2024-11-05 23:01:21 +0300

    create POV for FPGA

--- Wersall_Lione/Project_3/src/Omdazz_calcul_top.v
+++ Wersall_Lione/Project_3/src/Omdazz_calcul_top.v
@@ -16,7 +16,7 @@ output wire LED4    ,
 
 inout wire I2C_SCL  ,  //  I2C signal clk to EEPROM
 inout wire I2C_SDA  ,  //  I2C signal data write or read. 
-output wire BEEP    ,
+output wire beep    ,
 
 output wire PS_CLOCK,  // interfac PS2
 output wire PS_DATA ,  
@@ -106,7 +106,7 @@ inout wire SD_WE
 //SDRAM inout 
 //assign I2C_SCL = (1'b0) ? (1'b1) : (1'bZ); //3state - read
 
-assign {UART_TXD,BEEP,PS_CLOCK,PS_DATA,VGA_HSYNC,VGA_VSYNC,ASDO_EPCS4,
+assign {UART_TXD,PS_CLOCK,PS_DATA,VGA_HSYNC,VGA_VSYNC,ASDO_EPCS4,
 nCSO_EPCS4,DCLK_EPCS4,DIG_1,DIG_2,DIG_3,DIG_4} = 1'b1;
 assign {IR,VGA_B,VGA_G,VGA_R,SEG_0,SEG_1,SEG_3,SEG_4,SEG_5,SEG_6,SEG_7} = 1'b0;
 assign {I2C_SCL,I2C_SDA,SCL,SDA,S_DQ0,S_DQ1,S_DQ2,S_DQ3,S_DQ4,S_DQ5,S_DQ6,S_DQ7,
@@ -150,7 +150,7 @@ Buzzer_440Hz_inst
 ( 
 	.FPGA_CLK          (FPGA_CLK ), 
 	.sound_on              (flag3),
-    .beep                   (BEEP)
+    .beep                   (beep)
 );
 
 CALCUL
@@ -234,30 +234,36 @@ end
 
 endmodule
 
-module Buzzer( input wire FPGA_CLK, input wire sound_on,
-                output reg beep );
+module Buzzer( 
+input wire FPGA_CLK, 
+input wire sound_on,
+output reg beep 
+);
+
 reg [16:0] cnt_bzz;
 reg beep_bzz;
+
 initial begin
 cnt_bzz = 'd0;
 beep_bzz = 'd0;
 end
 
 always @(posedge FPGA_CLK) begin
-    if(posedge sound_on) begin
+    if(sound_on == 1'b1) begin
 	    beep <= beep_bzz;
 	end else begin
         beep <= beep;
 	end
 
 end
+
 always @(posedge FPGA_CLK) begin
     cnt_bzz <= cnt_bzz + 'd1;
     if (cnt_bzz == 'h1BBE5)begin // 440 Hz = 50.000.000 Hz / 113.637
-        beep_bzz; <= ~ beep_bzz;;
+        beep_bzz <= ~ beep_bzz;
         cnt_bzz <= 'd0;
     end else begin
-	    beep_bzz; <= beep_bzz;;
+	    beep_bzz <= beep_bzz;
     end
 end
 endmodule
